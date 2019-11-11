7d9f7904b97e96f9fc3cbd5ecc1e74ff4cc3c9f0
hanlin01 <hanlin01@qianxin.com>
2019-11-11 10:10:01 +0800

    groovy脚本插件

---
+++ data-rule-engine/src/main/java/com.han.rule/goovy/GroovyRuleEngine.java
@@ -0,0 +1,15 @@
+package com.han.rule.goovy;
+
+/**
+ * @author: Hanl
+ * @date :2019/11/7
+ * @desc:
+ */
+public interface GroovyRuleEngine<T> {
+
+    public void initEngine();
+
+    public T getGroovyRule(String id);
+
+    public void destroy();
+}
